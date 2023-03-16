199128a277b52e9b1755a90d5df343e42fadee2f
kakun45 <email4blackkat@gmail.com>
2023-03-16 16:01:51 -0400

    app fixed

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-
 require("dotenv").config();
-
 const { PORT } = process.env;
 app.use(cors());
 app.use(express.json());
 
-
-App.listen();
+app.listen();
